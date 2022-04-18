8a03d0806f830d69d4b70e8635b4ede741cd9402
kavinkrishk <kavinkrishk@gmail.com>
2022-04-18 14:15:46 +0530

    Ex1

---
+++ Git_ex1.py
@@ -0,0 +1,22 @@
+a = 5000
+print(a,"Available balance")
+b =(input("Enter Amount"))
+
+if a>=int(b):
+    c = ((int(a))*(int(b)))
+    d = a-(int(b))
+
+    if d==0:
+        print("Zero Balance")
+        print("Please maintain Minimum Balance")
+    elif d<=3000:
+        print(d,"Your Current Balance")
+        print("Please maintain Minimum Balance")
+    else:
+        print(d,"Your Current Balance")
+
+else:
+    print("Please Enter Numbers Only")
+
+
+
